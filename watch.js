e22fa79cdc2964c37131d448462a1cb55083b428
valentinrey <valrey@gmail.com>
2012-02-08 11:26:18 +0100

    Virkelig added jquery & watch.js!

---
+++ watch.js
@@ -0,0 +1,31 @@
+var seconds = 0;
+var minutes = 0;
+//called when the page loads to begin the timer
+function startTimer(){
+	//100 milliseconds = 1 second
+	window.setInterval("updateTime()", 1000);
+}
+
+//called every 100 milliseconds to update the timer
+function updateTime(){
+	++seconds;
+	var currentMinutes = "0" + minutes;
+	var currentSeconds = "0" + seconds;
+	if(currentSeconds >= 10){
+		currentSeconds = seconds;
+	}
+	if(currentMinutes >= 10){
+		currentMinutes = minutes;
+	}
+	while(seconds === 60){
+		seconds = 0;
+		minutes++;
+	}
+
+	var timeWatch = currentMinutes + ":" + currentSeconds;
+
+
+	document.getElementById("soFar").innerHTML = timeWatch;
+
+	document.getElementById("vis").innerHTML = $("#soFar").text();
+}
